b3fa28da158d94024b317ec6b55ee64182390edc
Bipin <bipinmnair89@gmail.com>
2024-06-10 11:39:35 +0530

    ES Class implementation of Object creation

--- objectcreations.js
+++ objectcreations.js
@@ -35,6 +35,8 @@ const objectUsingObjectLiterals = {
 console.log(objectUsingObjectLiterals);
 console.log(objectUsingObjectLiterals.addressDetails());
 
+console.log('*****************************************************************************************************************************************************************');
+
 
 /* Constructor Function
   -> It is a function that can be used to create multiple objects
@@ -50,12 +52,44 @@ function EmployeeDetails(name, company, designation) {
         return `Address Details of ${this.name} to be updated`;
     }
     this.toString = function() {
-        return `Employee Details\n Name: ${this.name}\n Company: ${this.company}\n Designation: ${this.designation}\n Address Details: ${this.addressDetails()}`
+        return `Employee Details\n Name: ${this.name}\n Company: ${this.company}\n Designation: ${this.designation}\n Address Details: ${this.addressDetails()}`;
     }
 }
 const employeeA = new EmployeeDetails('John', 'Google', 'Frontend Developer');
 console.log(employeeA.toString());
 const employeeB = new EmployeeDetails('Jane', 'Microsoft', 'Manager');
 console.log(employeeB.toString());
+
+console.log('*****************************************************************************************************************************************************************');
+
+
+/* ES6 class syntax
+  -> It is a class that can be used to create multiple objects
+  -> we need to use constructor keyword to define a constructor
+*/
+
+class EmployeeDetailsLibrary {
+
+        constructor(name, company, designation) {
+            this.name = name;
+            this.company = company;
+            this.designation = designation;
+            this.addressDetails = function() {
+                return `Address Details of ${this.name} to be updated`;
+            }
+            this.toString = function() {
+                return `Employee Details\n Name: ${this.name}\n Company: ${this.company}\n Designation: ${this.designation}\n Address Details: ${this.addressDetails()}`;
+            }
+        }
+}
+const personA = new EmployeeDetailsLibrary('Micheal', 'Netflix', 'Backend Developer');
+console.log(personA.toString());    
+const personB = new EmployeeDetailsLibrary('Sam', 'Amazon', 'Senior Developer');
+console.log(personB.toString());    
+
+console.log('*****************************************************************************************************************************************************************');
+
+
+
 
 
