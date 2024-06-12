2091da175680f8f36febbd1237c5ac42336a3899
Bipin <bipinmnair89@gmail.com>
2024-06-12 17:37:50 +0530

    Class concept in JJS

---
+++ classconcept.js
@@ -0,0 +1,26 @@
+/* This file is to understand class concept in Javascript
+  -> A .js file can contain multiple classes
+  -> Class is a blueprint for creating objects, objects
+  -> It is not necessary that .js file name and class name must be the same
+  -> In a class, constructor can be defined with the name constructor and there need not be a name
+  -> Constructor overloading is not possible in Javascript
+  -> Class can have constructors, getters, setters, static and non static methods.
+*/
+
+
+class Person {
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    printData() {
+        console.log(`Name: ${this.name}\nAge: ${this.age}\nAddress: ${this.address}`);
+    }
+}
+
+    const personA =new Person("John", 30, "New York");
+    const personB =new Person("Jane", 25, "London");
+
+
