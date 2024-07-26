baa4b306258bb3f8c451851e090d1b80278a02ac
Bipin <bipinmnair89@gmail.com>
2024-07-26 10:47:32 +0530

    Promises - basic & within function

---
+++ jsadvconcepts/promise/basicpromise.js
@@ -0,0 +1,21 @@
+//This is a basic promise implementation which checks a randomly generated function is odd or even.
+
+
+const myPromise = new Promise((resolve,reject) => {
+    //async code
+    setTimeout(() => {
+        const randomNumber = Math.floor(Math.random() * 100000000) + 1; //generating a positive number without decimal
+        if(randomNumber%2==0) {
+            resolve(randomNumber);
+        } else {
+            reject(new Error("Please try again..."));
+        }
+    });
+})
+
+//while using then catch finally, we should add semicolon only after finally.
+myPromise
+    .then(result => {console.log(`Number is even - ${result}`)})
+    .catch(error => {console.log(`Number is odd -  ${error}`)})
+    .finally(() => {console.log("Process completed")});
+
---
+++ jsadvconcepts/promise/promiseinsidefunc.js
@@ -0,0 +1,19 @@
+//This is a  promise implementation inside a function which checks a number passed as argument is odd or even.
+
+function checkOddOrEven(number, delay) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if(number%2==0) {
+                resolve(number);
+            }
+            else{
+                reject(new Error("Please try again..."));
+            }
+        }, delay);
+    });
+}
+
+checkOddOrEven(1,2000)
+    .then(result => {console.log(`Number is even - ${result}`)})
+    .catch(error => {console.log(`Number is odd -  ${error}`)})
+    .finally(() => {console.log("Process completed")});
