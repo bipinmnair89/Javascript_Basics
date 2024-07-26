d82185fc507f9e9fe13e3e39592c902e070145ee
Bipin <bipinmnair89@gmail.com>
2024-07-26 15:32:59 +0530

    Promise.allSettled

---
+++ jsadvconcepts/promise/promiseAllSettled.js
@@ -0,0 +1,36 @@
+
+
+function promiseA() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(`PromiseA`);
+        }, 2000);
+    });
+}
+
+function promiseB() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(`Promise B`);
+        }, 2000);
+    });
+}
+
+function promiseC() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            reject(new Error(`Error with Promise C`));
+        }, 5000);        //all promises would be moved to the event loop after 5 seconds (which is the maximum duration out of the three promises)
+    });
+}
+
+Promise.allSettled([promiseA(), promiseB(), promiseC()])
+    .then(result => {
+        result.forEach(res => {
+            if(res.status === 'fulfilled') {
+                console.log(res.value);
+            } else {
+                console.log(res.reason);
+            }
+        })
+    });
